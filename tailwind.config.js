aca8c42b743d23574b6190487b40d83410c83743
Alaa Nasser <alaanasser053@gmail.com>
2021-04-12 09:21:55 +0200

    new deploy

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    colors: {
+      heading: '#1f1f1f',
+      texts: '#22f3f',
+      buttons: '#1290a4',
+      learn: '#1290A7',
+      paragraphs: '#666666',
+      popular: '#f46f7c',
+      unavilable: '#8e8e8e',
+      para: '#878b90',
+      buttonhover: '#0e7282',
+      circles: '#FEF0F2',
+      third: '#F8F8F8',
+      fourth: '#FEFDFD',
+      fifth: '#30393B',
+    },
+    fontFamily: {
+      karla: ['Karla']
+    },
+    backgroundColor: {
+      offwhite: '#F8F8F8',
+      searchbox: '#ebf2f3'
+    },
+    spacing:{
+      veryhigh: '40em'
+    }
+  },
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
